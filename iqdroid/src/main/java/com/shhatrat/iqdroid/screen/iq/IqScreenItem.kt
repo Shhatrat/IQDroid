269874765f8046f2915c943f78ff2f73ffa98487
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2020-02-03 17:40:15 +0100

    fix adding screens

--- iqdroid/src/main/java/com/shhatrat/iqdroid/screen/iq/IqScreenItem.kt
+++ iqdroid/src/main/java/com/shhatrat/iqdroid/screen/iq/IqScreenItem.kt
@@ -19,28 +19,6 @@ sealed class IqScreenItem(
         return Gson().toJson(this)
     }
 
-    enum class ShortName(val shortCode: String) {
-        TYPE_TEXT("DT"),
-        TYPE_BACKGROUND("BG"),
-        TYPE_LINE("LN"),
-        TYPE_CIRCLE("CR"),
-        TYPE_ECLIPSE("EL"),
-        TYPE_RECTANGLE("RT"),
-        TYPE_RECTANGLE_ROUNDED("RTR"),
-        FONT("f"),
-        TEXT("t"),
-        JUSTIFICATION("j"),
-        X1("x1"),
-        Y1("y1"),
-        X2("x2"),
-        Y2("y2"),
-        HEIGHT("h"),
-        WIDTH("w"),
-        RADIUS("r"),
-        A("a"),
-        B("b")
-    }
-
     class Text(
         x: Int,
         y: Int,
